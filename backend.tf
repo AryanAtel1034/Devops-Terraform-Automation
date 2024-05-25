bba995e53a99d1c86d4fc61d9b76906d0dad8c2d
aryanatel <atelaryan@gmail.coom>
2024-05-25 15:02:29 +0530

    First File Original

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "mydev-tf-state-bucket-project-terraform-batch-23-atel"
+    key = "main"
+    region = "us-east-1"
+    dynamodb_table = "my-dynamodb-table"
+  }
+}
