847ad2456923e492a2ac5e3b691e9051ef0ad1e6
aryanatel <atelaryan@gmail.coom>
2024-06-01 16:30:06 +0530

    Done Final

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform-batch-23-AryanAtel1034"
+    bucket = "mydev-tf-state-bucket-project-terraform-batch-23-Patel"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
+    aaddd 
   }
 }
