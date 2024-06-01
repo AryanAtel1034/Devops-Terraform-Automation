ac214d8742cdaede86478e55ec024a6b26fc0ed1
aryanatel <atelaryan@gmail.coom>
2024-06-01 16:19:07 +0530

    Bucket name change

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform-batch-23-atel"
+    bucket = "mydev-tf-state-bucket-project-terraform-batch-23-AryanAtel"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
